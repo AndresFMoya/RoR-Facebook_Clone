365eaaa37bd170ed4e164c813800995e9bba0ab2
AndresFMoya <andresfmoya@gmail.com>
2019-11-25 15:02:21 -0500

    chore: update like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,14 +4,18 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @like = current_user.likes.build
-    return unless @like.save
-
+    @like = current_user.likes.build(like_params)
+    flash[:success] = 'You like this post!' if @like.save
     redirect_to root_url
   end
 
   def destroy
     @like.destroy
+    flash[:success] = 'You unlike this post!'
     redirect_to root_url
+  end
+
+  def like_params
+    params.require(:like).permit(:post_id)
   end
 end
