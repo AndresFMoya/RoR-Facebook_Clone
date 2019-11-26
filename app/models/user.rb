094d3c9f1c036659dd5e2245e59da709306fc94b
KerronKing <king_of_palmiste@hotmail.com>
2019-11-26 17:18:14 -0400

    Chore: add associations between user and friendship models

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+  has_many :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, length: { maximum: 40 }
 end
