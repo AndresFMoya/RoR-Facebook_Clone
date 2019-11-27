1893348a2703712adc384e0bcfd35b005f76868c
KerronKing <king_of_palmiste@hotmail.com>
2019-11-27 13:08:47 -0400

    Update: add create and update actions to friendship controller and edit user index view

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,29 +1,34 @@
 class FriendshipsController < ApplicationController
   before_action :authenticate_user!
 
-  # def create
-  #   @friendship = 
-  #   if @friendship.save
-  #     flash[:success] = 'Friend request sent!'
-  #   else
-  #     flash[:danger] = "Error sending request"
-  #   end
-  #   redirect_back(fallback_location: root_path)
-  # end
+  def create
+    @friendship = Friendship.new(friendship_params)
+    if @friendship.save
+      flash[:success] = "Friend request sent!"
+    else
+      flash[:danger] = "Error sending request"
+    end
+    redirect_back(fallback_location: root_path)
+  end
 
-  # def update
-  #   @post = current_user.posts.build(post_params)
-  #   if @post.save
-  #     flash[:success] = 'Post created!'
-  #   else
-  #     flash[:danger] = "Post content can't be blank"
-  #   end
-  #   redirect_back(fallback_location: root_path)
-  # end
+  def update
+    if @friendship.update
+      flash[:success] = 'Friend Accepted!'
+    else
+      flash[:danger] = "Error accepting friend"
+    end
+    redirect_back(fallback_location: root_path)
+  end
 
   # def destroy
   #   @post.destroy
   #   flash[:success] = 'Post deleted'
   #   redirect_back(fallback_location: root_path)
+  # end
+
+  private
+
+  def friendship_params
+    params.require(:friendship).permit(:user_id, :friend_id, :confirmation)
   end
 end
