90a63ea345a7d58a27cdc12d67d2552f9ad4fa35
KerronKing <king_of_palmiste@hotmail.com>
2019-11-28 13:18:21 -0400

    Test: add friendship unit and feature tests

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,16 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(name: "Laura", email: "laura@example.com", password: "laura123") }
+  let(:friend) { User.new(name: "Marsha", email: "marsha@example.com", password: "marsha123") }
+
+  it "should have one user" do
+    friendship = Friendship.reflect_on_association(:user)
+    expect(friendship.macro).to eq(:belongs_to)
+  end
+
+  it "should have one friend" do
+    friendship = Friendship.reflect_on_association(:friend)
+    expect(friendship.macro).to eq(:belongs_to)
+  end
 end
