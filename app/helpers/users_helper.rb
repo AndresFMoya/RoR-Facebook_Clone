1bfff85693b311e529c20ef3348ea58dad15732c
AndresFMoya <andresfmoya@gmail.com>
2019-11-28 14:49:24 -0500

    style: add gravatar image to users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module UsersHelper
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user)
+    gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
