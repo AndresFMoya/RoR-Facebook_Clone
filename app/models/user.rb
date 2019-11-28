8b031d8fb6cf5ec1f3138bc55eccc50c27ff199a
AndresFMoya <andresfmoya@gmail.com>
2019-11-28 17:45:46 -0500

    chore: delete unnecesary queries in friendship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,6 @@ class User < ApplicationRecord
 
   def friends
     friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-    friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.confirmed }
     friends_array.compact
   end
 
@@ -27,21 +26,12 @@ class User < ApplicationRecord
     inverse_friendships.map { |friendship| friendship.user unless friendship.confirmed }.compact
   end
 
-  def confirm_friend(user)
-    friendships = inverse_friendships.find { |friendship| friendship.user == user }
-    friendships.confirmed = true
-    friendships.save
-  end
-
   def friend?(user)
     friends.include?(user)
   end
 
   def newsfeed
     friend_ids = 'SELECT friend_id FROM friendships WHERE user_id = :user_id'
-    inverse_friend_ids = 'SELECT user_id FROM friendships WHERE friend_id = :user_id'
     newsfeed_friends = Post.where("user_id IN (#{friend_ids}) OR user_id = :user_id", user_id: id)
-    newsfeed_inverse_friends = Post.where("user_id IN (#{inverse_friend_ids})", user_id: id)
-    newsfeed_friends + newsfeed_inverse_friends
   end
 end
