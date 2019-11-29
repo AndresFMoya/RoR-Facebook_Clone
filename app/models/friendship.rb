f6a9fbc4fd50d7db8891ac3e0f2ae573be125470
AndresFMoya <andresfmoya@gmail.com>
2019-11-29 10:47:42 -0500

    chore: add validations to friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,17 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  validates :user_id, presence: true, uniqueness: { scope: :friend_id }
+  validates :friend_id, presence: true
+  validates :friend_id, exclusion: {in: ->(u) { [u.user_id] }}
+  validate :users_are_not_already_friends
+
+  def users_are_not_already_friends
+    combinations = ["user_id = #{user_id} AND friend_id = #{friend_id}",
+                    "user_id = #{friend_id} AND friend_id = #{user_id}"]
+    if Friendship.where(combinations.join(' OR ')).exists?
+      self.errors.add(:user_id, 'Already friends!')
+    end
+  end
 end
