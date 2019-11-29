0f31cf33a85752f588ebbec94e1531f4f24aa9d4
AndresFMoya <andresfmoya@gmail.com>
2019-11-29 11:27:49 -0500

    chore: edit newsfeed method on user model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -6,14 +6,13 @@ class Friendship < ApplicationRecord
 
   validates :user_id, presence: true, uniqueness: { scope: :friend_id }
   validates :friend_id, presence: true
-  validates :friend_id, exclusion: {in: ->(u) { [u.user_id] }}
+  validates :friend_id, exclusion: { in: ->(u) { [u.user_id] } }
+
   validate :users_are_not_already_friends
 
   def users_are_not_already_friends
-    combinations = ["user_id = #{user_id} AND friend_id = #{friend_id}",
-                    "user_id = #{friend_id} AND friend_id = #{user_id}"]
-    if Friendship.where(combinations.join(' OR ')).exists?
-      self.errors.add(:user_id, 'Already friends!')
-    end
+    combinations = ["user_id = #{user_id} AND friend_id = #{friend_id} AND confirmed = true",
+                    "user_id = #{friend_id} AND friend_id = #{user_id} AND confirmed = true"]
+    errors.add(:user_id, 'Already friends!') if Friendship.where(combinations.join(' OR ')).exists?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
-  has_many :friendships
-  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :friendships, dependent: :destroy
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id', dependent: :destroy
   validates :name, presence: true, length: { maximum: 20 }
   validates :email, presence: true, length: { maximum: 40 }
 
@@ -38,8 +38,8 @@ class User < ApplicationRecord
   end
 
   def newsfeed
-    friend_ids = 'SELECT friend_id FROM friendships WHERE user_id = :user_id'
-    inverse_friend_ids = 'SELECT user_id FROM friendships WHERE friend_id = :user_id'
+    friend_ids = 'SELECT friend_id FROM friendships WHERE user_id = :user_id AND confirmed = true'
+    inverse_friend_ids = 'SELECT user_id FROM friendships WHERE friend_id = :user_id AND confirmed = true'
     newsfeed_friends = Post.where("user_id IN (#{friend_ids}) OR user_id = :user_id", user_id: id)
     newsfeed_inverse_friends = Post.where("user_id IN (#{inverse_friend_ids})", user_id: id)
     newsfeed_friends + newsfeed_inverse_friends
