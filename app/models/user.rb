f7d8381711e5fb41069f5277041dc40a3a4be0cb
AndresFMoya <andresfmoya@gmail.com>
2019-11-30 08:20:37 -0500

    style: fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,10 +18,7 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name   # assuming the user model has a name
-      # If you are using confirmable and the provider(s) you use validate emails,
-      # uncomment the line below to skip the confirmation emails.
-      # user.skip_confirmation!
+      user.name = auth.info.name
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  resources :users, only: [:show, :index]
+  resources :users, only: %i[show index]
   resources :comments
   resources :likes
   resources :posts
