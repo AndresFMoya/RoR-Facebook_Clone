9a56bf40cca692713c5817115e61c94a4f6f2ad5
KerronKing <king_of_palmiste@hotmail.com>
2019-12-02 17:42:39 -0400

    test: add facebook login feature test

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,7 +59,6 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name
-      user.image = auth.info.image
     end
   end
 
---
+++ spec/features/facebook_login_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "FacebookLogins", type: :feature do
+  before :each do
+    mock_auth_hash
+    Rails.application.env_config["devise.mapping"] = Devise.mappings[:user] # If using Devise
+    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+  end
+
+  scenario "authenticates successfully" do
+    visit "/users/sign_in"
+    click_on "Sign in with Facebook"
+    expect(page).to have_content("Successfully authenticated from Facebook account.")
+  end
+end
