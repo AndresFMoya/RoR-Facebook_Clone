94e605115554db8bc9a343e6db1c080607450d8a
AndresFMoya <andresfmoya@gmail.com>
2019-12-02 15:32:30 -0500

    chore: update the friendship table to require confirmed value always

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2019_11_26_204605) do
   create_table 'friendships', force: :cascade do |t|
     t.bigint 'user_id'
     t.bigint 'friend_id'
-    t.boolean 'confirmed'
+    t.boolean 'confirmed', default: false, null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
     t.index ['friend_id'], name: 'index_friendships_on_friend_id'
