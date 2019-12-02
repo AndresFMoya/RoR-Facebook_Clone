d630e5fb88056b62ccce723d6274a40e473a4eb2
AndresFMoya <andresfmoya@gmail.com>
2019-12-02 18:05:00 -0500

    fix: delete duplicate migration

--- db/migrate/20191129170457_add_omniauth_to_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddOmniauthToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
-  end
-end
